d1631807094eca71d36145aab86a4a04bfc32dd4
Sophie Kuchapina <122675180+RozaVii@users.noreply.github.com>
2023-10-23 19:11:26 +1000

    Update GeneralCode.py

--- GeneralCode.py
+++ GeneralCode.py
@@ -5,14 +5,14 @@ def generate_java_code(VB_ast):
         # Генерация заголовка программы
         program_name = VB_ast.children[0].value
         java_code += f"#include <iostream>\n\n"
-        java_code += f"int main() {{\n"
+        java_code += f"int main()\n"
 
         # Генерация остальной части программы
         statement_list = VB_ast.children[0]
         java_code += generate_java_code(statement_list)
 
         # Завершение программы
-        java_code += f"\n    return 0;\n }}"
+        java_code += f"\n    return 0;\n"
 
     elif VB_ast.type == "STATEMENT_LIST":
         for statement in VB_ast.children:
@@ -22,12 +22,19 @@ def generate_java_code(VB_ast):
         condition = generate_java_code(VB_ast.children[0])
         if_body = generate_java_code(VB_ast.children[1])
         else_body = generate_java_code(VB_ast.children[2])
-        java_code += f"if ({condition}) {{\n"
+        java_code += f"If ({condition}) \n"
         java_code += f"{if_body}\n"
-        java_code += f"}}"
+        java_code += f"End If"
         if else_body:
-            java_code += f" else {{\n"
+            java_code += f" else \n"
             java_code += f"{else_body}\n"
-            java_code += f"}}\n"
+            java_code += f"End If\n"
 
-            return java_code
+    elif VB_ast.type == "WHILE_STATEMENT":
+        condition = generate_java_code(pascal_ast.children[0])
+        loop_body = generate_java_code(pascal_ast.children[1])
+        java_code += f"While ({condition}) \n"
+        java_code += f"{loop_body}\n"
+        java_code += f"End While\n"
+
+        return java_code

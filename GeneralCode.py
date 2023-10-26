32201b008f8f57ae6c2de14060d3c7b3ab92972d
Eugenia Fen <95513839+EugeneFen@users.noreply.github.com>
2023-10-26 11:38:51 +1000

    Update GeneralCode.py

--- GeneralCode.py
+++ GeneralCode.py
@@ -2,6 +2,10 @@ from parse import Parser
 from lexer import Lexer
 
 class General_code:
+    def error(self, message):
+        print("Error: ", message)
+        sys.exit(1)
+
     def generate_java_code(self, VB_ast):
         java_code = ""
 
@@ -12,7 +16,7 @@ class General_code:
                 self.generate_java_code(VB_ast.children[i])
 
         elif VB_ast.node_type == Parser.IF_STATEMENT:
-            condition = self.generate_java_code(VB_ast.children[0])
+            condition = self.get_code_type(VB_ast.children[0])
             if_body = self.generate_java_code(VB_ast.children[1])
             java_code += f"If ({condition}) \n"
             java_code += "{"
@@ -23,18 +27,28 @@ class General_code:
                 java_code += f" else \n"
                 java_code += f"{else_body}\n"
 
-        elif VB_ast.node_type == Parser.ID or \
+        elif VB_ast.node_type == Parser.ADDITION:
+            value_term = self.generate_java_code(VB_ast.children[0])
+            value_right = self.generate_java_code(VB_ast.children[1])
+            java_code += f"{value_term} + {value_right} \n"
+
+        elif VB_ast.node_type == Parser.EXPRESSION_STATEMENT:
+            for i in range(len(VB_ast.children)):
+                self.generate_java_code(VB_ast.children[i])
+
+        print( java_code)
+
+    def get_code_type(self, VB_ast):
+        if VB_ast.node_type == Parser.ID or \
               VB_ast.node_type == Parser.INTEGER or \
               VB_ast.node_type == Parser.DOUBLE or \
               VB_ast.node_type == Parser.STRING or \
               VB_ast.node_type == Parser.CHAR or \
               VB_ast.node_type == Parser.TRUE or \
               VB_ast.node_type == Parser.FALSE:
-            java_code += f"{VB_ast.value}  "
+            code = VB_ast.value
+            print(VB_ast.value, "Type \n")
 
-        elif VB_ast.node_type == Parser.ADDITION:
-            value_term = self.generate_java_code(VB_ast.children[0])
-            value_right = self.generate_java_code(VB_ast.children[1])
-            java_code += f"{value_term} + {value_right} \n"
-
-        return java_code
+            return code
+        else: 
+            self.error(self.current_token.type)
